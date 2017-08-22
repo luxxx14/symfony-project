98ad506aa947a6d430a30369be8c41cd81756f17
RiverFisher <goldenarius@gmail.com>
2017-08-22 22:46:09 +1000

    Uniting of the changes.

---
+++ src/Management/AdminBundle/Menu/Builder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Management\AdminBundle\Menu;
+
+use Knp\Menu\FactoryInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+class Builder implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function mainMenu(FactoryInterface $factory, array $options)
+    {
+        $menu = $factory->createItem('root', ['childrenAttributes' => ['class' => 'sidebar-menu']]);
+
+        $menu->addChild('Статичные блоки', ['attributes' => ['class' => 'header']]);
+        $menu->addChild('Общая информация', ['route' => 'admin_common_information_index']);
+        $menu->addChild('Компоненты платформы', ['route' => 'admin_component_index']);
+        $menu->addChild('Основные преимущества', ['route' => 'admin_advantage_index']);
+        $menu->addChild('Кто использует', ['route' => 'admin_client_index']);
+        $menu->addChild('О компании', ['route' => 'admin_company_information_index']);
+
+        $menu->addChild('Maven', ['attributes' => ['class' => 'header']]);
+        $menu->addChild('Коды для Maven', ['route' => 'admin_version_index']);
+
+        $menu->addChild('Новости', ['attributes' => ['class' => 'header']]);
+        $menu->addChild('Настройка источника RSS', array(
+            'route' => 'admin_redirect_for_selected_feed_source',
+            'routeParameters' => array('action' => 'show')
+        ));
+        $menu->addChild('Модерация новостей', array(
+            'route' => 'admin_redirect_for_selected_feed_source',
+            'routeParameters' => array('action' => 'show_feed')
+        ));
+
+        $menu->addChild('Подписчики', ['attributes' => ['class' => 'header']]);
+        $menu->addChild('Список подписчиков', ['route' => 'admin_subscriber_index']);
+
+        return $menu;
+    }
+}
