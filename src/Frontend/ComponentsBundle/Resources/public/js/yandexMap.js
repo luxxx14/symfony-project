c1f12bedddec8b072da81590ebf2cde56706d23e
Viktor Sorochuk <vsorochuk@axeta.ru>
2017-08-22 17:14:58 +0300

    Fix small issues

--- src/Frontend/ComponentsBundle/Resources/public/js/yandexMap.js
+++ src/Frontend/ComponentsBundle/Resources/public/js/yandexMap.js
@@ -3,13 +3,14 @@ var myMap;
 var myPlacemark;
 
 function init(){ 
+		var address = document.querySelector('.address').innerText;
 		myMap = new ymaps.Map("map", {
 				center: [55.771403, 37.714902],
 				zoom: 15
 		}); 
 
 		myPlacemark = new ymaps.Placemark([55.771403, 37.714902], {
-				hintContent: ' Боровая ул., дом 7, корп. 2',
+				hintContent: address,
 				balloonContent: 'КУРС'
 		});
 
