26dfc906d2bca0d5017c634de6984408b47e641f
RiverFisher <goldenarius@gmail.com>
2017-09-15 22:52:19 +1000

    All dumps was added.

--- ansible/roles/webpage/files/dump_create.sql
+++ ansible/roles/webpage/files/dump_create.sql
@@ -15,6 +15,26 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
+-- Name: corchestra_db; Type: DATABASE; Schema: -; Owner: postgres
+--
+
+CREATE DATABASE corchestra_db WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+ALTER DATABASE corchestra_db OWNER TO postgres;
+
+\connect corchestra_db
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+--
 -- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
 --
 
