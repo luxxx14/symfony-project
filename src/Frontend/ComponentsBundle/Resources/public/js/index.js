5fd021b6ef020e51515c1339372c11f4a554fa45
Masiya Guseynov <mguseynov@axeta.ru>
2018-02-13 12:06:22 +0300

    Download section fixes

--- src/Frontend/ComponentsBundle/Resources/public/js/index.js
+++ src/Frontend/ComponentsBundle/Resources/public/js/index.js
@@ -146,13 +146,21 @@ $(document).ready(function(){
 
 	//show dropdown lists
 	$('.show-components .show-components__text, .download-versions__item-icon').on('click', function() {
-		if(parseInt($(this).closest('.show-components').find('.show-components__text').css('marginBottom')) > 0) {
-			$(this).closest('.show-components').find('.show-components__text').css('marginBottom', 0);
-		} else {
-			$(this).closest('.show-components').find('.show-components__text').css('marginBottom', $(this).closest('.show-components').find('.components').css('height'));
-		}
+		var $showComponents = $(this).closest('.show-components');
+		var $showComponentsText = $showComponents.find('.show-components__text');
+		var $showComponentsList = $showComponents.find('.components');
 
-		$(this).closest('.show-components').find('.components').toggle();
+		if(parseInt($showComponentsText.css('marginBottom')) > 0) {
+			$showComponentsText.css('marginBottom', 0);
+		} else {
+			$showComponentsText.css('marginBottom', $showComponentsList.css('height'));
+		}
+		if($showComponentsList.css('display') == 'block') {
+			$showComponentsText.removeClass('active');
+		}else {
+			$showComponentsText.addClass('active');
+		}
+		$showComponentsList.toggle();
 	});
 });
 
